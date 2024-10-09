0293714dbac108bbd0c7d05a97b0fa2dc9676b8a
Mike Bostock <mbostock@gmail.com>
2024-10-09 15:49:05 -0700

    checkpoint

--- src/@[scope]/[name]/index.md.js
+++ src/@[scope]/[name]/index.md.js
@@ -1,5 +1,9 @@
 import {parseArgs} from "node:util";
+import {sum} from "d3-array";
+import {utcDay} from "d3-time";
+import {format as formatIso} from "isoformat";
 import {github} from "../../github.js";
+import {getNpmDownloadsByDate, getNpmDownloadsByVersion, getNpmPackage} from "../../npm.js";
 
 const {
   values: {scope, name}
@@ -7,11 +11,40 @@ const {
   options: {scope: {type: "string"}, name: {type: "string"}}
 });
 
-// TODO lookup github repo from npm package name…
-const {body: info} = await github(`/repos/${scope}/${name}`);
+// TODO This assumes that the GitHub repo name corresponds to the npm package
+// name (and scope), but that isn’t guaranteed. We could either create a set of
+// manual overrides here (e.g., for htl), or we could fetch the package.json
+// from the GitHub repo and find the corresponding npm package name.
 
-// TODO get releases from npm instead of github
-const {body: [release]} = await github(`/repos/${scope}/${name}/releases?per_page=1`);
+const today = utcDay();
+
+const [{body: info}, info2, downloads, versions] = await Promise.all([
+  github(`/repos/${scope}/${name}`),
+  getNpmPackage(`@${scope}/${name}`),
+  getNpmDownloadsByDate(`@${scope}/${name}`),
+  getNpmDownloadsByVersion(`@${scope}/${name}`)
+]);
+
+const data = [];
+
+for (const version in info2.versions) {
+  if (isPrerelease(version)) continue;
+  data.push({
+    version,
+    create_time: info2.time[version] ? new Date(info2.time[version]) : undefined,
+    downloads: versions.downloads[version]
+  });
+}
+
+data.sort((a, b) => a.create_time - b.create_time);
+
+function replacer(key, value) {
+  return value && /(_|^)(date|time)$/.test(key) ? formatIso(new Date(value)) : value;
+}
+
+function isPrerelease(version) {
+  return /-/.test(version);
+}
 
 process.stdout.write(`# [@${scope}/${name}](https://github.com/${scope}/${name})
 
@@ -30,15 +63,28 @@ ${info.description}
   </a>
   <a href=https://github.com/${scope}/${name}/releases class="card">
     <h2>Latest release</h2>
-    <div class="big">${release?.name}</div>
+    <div class="big">${info2["dist-tags"].latest}</div>
+  </a>
+  <a href=https://github.com/${scope}/${name}/releases class="card">
+    <h2>Days since last release</h2>
+    <div class="big">${utcDay.count(new Date(info2.time[info2["dist-tags"].latest]), today)}</div>
   </a>
   <a class="card">
     <h2>npm downloads (last 7 days)</h2>
-    <div class="big">$\{d3.sum(downloads.slice(0, 7), (d) => d.value).toLocaleString("en-US")}</div>
+    <div class="big">${sum(downloads.slice(0, 7), (d) => d.value).toLocaleString("en-US")}</div>
   </a>
 </div>
 
 ---
+
+~~~js
+const downloads = JSON.parse(data_downloads.textContent, reviver);
+const versions = JSON.parse(data_versions.textContent, reviver);
+
+function reviver(key, value) {
+  return typeof value === "string" && /(^|_)(date|time)$/.test(key) ? new Date(value) : value;
+}
+~~~
 
 ~~~js
 Plot.plot({
@@ -72,28 +118,12 @@ TODO
 - github issue burndown chart
 - github commits by author timeline chart
 - github calendar commits
+- inline data rather than using FileAttachment
 
 ---
 
-~~~js
-const downloads = FileAttachment("./downloads.json").text().then((text) => JSON.parse(text, reviver));
-~~~
-~~~js
-const versions = FileAttachment("./versions.json").text().then((text) => JSON.parse(text, reviver));
-~~~
-~~~js
-function reviver(key, value) {
-  return typeof value === "string" && /(^|_)(date|time)$/.test(key) ? new Date(value) : value;
-}
-~~~
-
-~~~js
-(${JSON.stringify(info)})
-~~~
-
-~~~js
-(${JSON.stringify(release)})
-~~~
+<script type="application/json" id="data_downloads">${JSON.stringify(downloads, replacer)}</script>
+<script type="application/json" id="data_versions">${JSON.stringify(data, replacer)}</script>
 
 <style type="text/css">
 
