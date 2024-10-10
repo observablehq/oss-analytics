38f3df14c54bd7969dd4cca5fb87ade2e912e8f2
Mike Bostock <mbostock@gmail.com>
2024-10-09 18:00:31 -0700

    checkpoint

--- src/@[scope]/[name]/index.md.js
+++ src/@[scope]/[name]/index.md.js
@@ -2,7 +2,7 @@ import {parseArgs} from "node:util";
 import {sum} from "d3-array";
 import {utcDay} from "d3-time";
 import {format as formatIso} from "isoformat";
-import {github} from "../../github.js";
+import {github, githubList} from "../../github.js";
 import {getNpmDownloadsByDate, getNpmDownloadsByVersion, getNpmPackage} from "../../npm.js";
 
 const {
@@ -25,6 +25,17 @@ const [{body: githubInfo}, npmInfo, downloads, downloadsByVersion] = await Promi
   getNpmDownloadsByVersion(`@${scope}/${name}`)
 ]);
 
+const commits = [];
+
+for await (const commit of githubList(`/repos/${scope}/${name}/commits`, {reverse: false})) {
+  commits.push({
+    sha: commit.sha,
+    message: truncate(commit.commit.message),
+    date: commit.commit.committer.date,
+    author: commit.author?.login
+  });
+}
+
 const versions = [];
 
 for (const version in npmInfo.versions) {
@@ -37,6 +48,11 @@ for (const version in npmInfo.versions) {
 }
 
 versions.sort((a, b) => a.date - b.date);
+
+function truncate(message, length = 255) {
+  message = message.replace(/\n\n.*/s, "");
+  return message.length <= length ? message : `${message.slice(0, length - 1)}…`;
+}
 
 function replacer(key, value) {
   return value && /(_|^)(date|time)$/.test(key) ? formatIso(new Date(value)) : value;
@@ -69,6 +85,10 @@ ${githubInfo.description}
     <h2>Days since last release</h2>
     <div class="big">${utcDay.count(new Date(npmInfo.time[npmInfo["dist-tags"].latest]), today)}</div>
   </a>
+  <a href=https://github.com/${scope}/${name}/commits class="card">
+    <h2>Days since last commit</h2>
+    <div class="big">${utcDay.count(new Date(commits[0].date), today)}</div>
+  </a>
   <a class="card">
     <h2>npm downloads (last 7 days)</h2>
     <div class="big">${sum(downloads.slice(0, 7), (d) => d.value).toLocaleString("en-US")}</div>
@@ -78,8 +98,10 @@ ${githubInfo.description}
 ---
 
 ~~~js
-const downloads = JSON.parse(data_downloads.textContent, reviver);
-const versions = JSON.parse(data_versions.textContent, reviver);
+const downloads = JSON.parse(data__downloads.textContent, reviver);
+const versions = JSON.parse(data__versions.textContent, reviver);
+const commits = JSON.parse(data__commits.textContent, reviver);
+const domain = [d3.utcDay(commits.at(-1).date), new Date("${formatIso(today)}")];
 
 function reviver(key, value) {
   return typeof value === "string" && /(^|_)(date|time)$/.test(key) ? new Date(value) : value;
@@ -95,6 +117,9 @@ function reviver(key, value) {
 Plot.plot({
   width,
   height: 400,
+  marginLeft: 0,
+  marginRight: 60,
+  x: {domain},
   y: {label: "downloads"},
   marks: [
     Plot.axisY({anchor: "right", label: null}),
@@ -121,13 +146,13 @@ Plot.plot({
 Plot.plot({
   width,
   label: null,
+  marginLeft: 40,
+  marginRight: 60,
   x: {axis: "top", grid: true},
-  color: {scheme: "reds", reverse: true},
   marks: [
     Plot.barX(versions, {
       y: "version",
       x: "downloads",
-      fill: "date",
       sort: {y: "x", reverse: true, limit: 10}
     }),
     Plot.text(versions, {
@@ -148,6 +173,31 @@ Plot.plot({
 
 ---
 
+<div class="grid grid-cols-1">
+  <div class="card">
+    <h2>Commits by author</h2>
+    <h3>Top 10 authors</h3>
+
+~~~js
+Plot.plot({
+  width,
+  label: null,
+  marginLeft: 0,
+  marginRight: 60,
+  x: {axis: "top", domain},
+  y: {grid: true},
+  marks: [
+    Plot.axisY({anchor: "right", textOverflow: "ellipsis-middle", lineWidth: 5}),
+    Plot.dot(commits, {x: "date", y: "author", href: (d) => \`https://github.com/${scope}/${name}/commit/$\{d.sha\}\`, target: "_blank", title: "message", tip: true, sort: {y: "x", reduce: "count", reverse: true, limit: 10}})
+  ]
+})
+~~~
+
+  </div>
+</div>
+
+---
+
 TODO
 
 - npm downloads big number ☑️
@@ -159,17 +209,20 @@ TODO
 - github stars ☑️
 - github stars last seven days
 - days since last release ☑️
-- days since last commit
+- days since last commit ☑️
 - days since last issue or discussion activity
 - github open issues big number
 - github issue burndown chart
 - github top issues
-- github commits by author timeline chart
-- github calendar commits
-- inline data rather than using FileAttachment
+- github commits by author timeline chart ☑️
+- github commits calendar heatmap
+- github top contributors
+- inline data rather than using FileAttachment ☑️
+- don’t show more than three years?
 
-<script type="application/json" id="data_downloads">${JSON.stringify(downloads, replacer)}</script>
-<script type="application/json" id="data_versions">${JSON.stringify(versions, replacer)}</script>
+<script type="application/json" id="data__downloads">${JSON.stringify(downloads, replacer)}</script>
+<script type="application/json" id="data__versions">${JSON.stringify(versions, replacer)}</script>
+<script type="application/json" id="data__commits">${JSON.stringify(commits, replacer)}</script>
 
 <style type="text/css">
 
