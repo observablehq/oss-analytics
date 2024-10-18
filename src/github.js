ab6ec14093c672c5771b820202af1f3e6aacb4b7
Mike Bostock <mbostock@gmail.com>
2024-10-18 00:03:23 -0700

    handle rate limits?

--- src/github.js
+++ src/github.js
@@ -3,6 +3,8 @@ import {fetchCached as fetch} from "./fetch.js";
 
 const {GITHUB_TOKEN} = process.env;
 
+let ratelimitReset;
+
 export async function fetchGithub(path, options) {
   return (await requestGithub(path, options)).body;
 }
@@ -16,8 +18,26 @@ export async function requestGithub(
 ) {
   const url = new URL(path, "https://api.github.com");
   const headers = {...(authorization && {authorization}), accept};
-  const response = await fetch(url, {headers});
-  if (!response.ok) throw new Error(`failed to fetch ${url}: ${response.status}`);
+  let response;
+  for (let attempt = 0, maxAttempts = 3; attempt < maxAttempts; ++attempt) {
+    if (ratelimitReset) {
+      console.warn(`x-ratelimit-reset ${ratelimitReset}`);
+      const ratelimitDelay = new Date(ratelimitReset * 1000) - Date.now();
+      await new Promise((resolve) => setTimeout(resolve, ratelimitDelay));
+      ratelimitDelay = null;
+    }
+    response = await fetch(url, {headers});
+    const headers = response.headers;
+    if (headers["x-ratelimit-remaining"] === "0") ratelimitReset = headers["x-ratelimit-reset"];
+    if (response.ok) break;
+    if (headers["retry-after"]) {
+      console.warn(`retry-after ${retryAfter}`);
+      const retryDelay = retryAfter * 1000;
+      await new Promise((resolve) => setTimeout(resolve, retryDelay));
+      continue;
+    }
+    throw new Error(`failed to fetch ${url}: ${response.status}`);
+  }
   return {headers: response.headers, body: await response.json()};
 }
 
